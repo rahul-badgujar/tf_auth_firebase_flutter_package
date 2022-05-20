123ef22bbfbe50c25a1561d1c4942bafb7dcfdaf
Rahul Badgujar <rahulbadgujar1508@gmail.com>
2022-05-20 17:16:00 +0530

    impl

--- lib/src/tf_auth_firebase_impl.dart
+++ lib/src/tf_auth_firebase_impl.dart
@@ -1,3 +1,34 @@
 import 'package:tf_auth_base/tf_auth_base.dart';
 
-class TfAuthFirebase extends TfAuth {}
+class TfAuthFirebase extends TfAuth {
+  @override
+  Future<void> loginWithEmailPassword(
+      {required String email, required String password}) {
+    // Firebase specific implementation here
+    return Future<void>.value();
+  }
+
+  @override
+  Future<void> loginWithEmailLink({required String email}) {
+    // Firebase specific implementation here
+    return Future<void>.value();
+  }
+
+  @override
+  Future<void> loginWithApple() {
+    // Firebase specific implementation here
+    return Future<void>.value();
+  }
+
+  @override
+  Future<void> loginWithFacebook() {
+    // Firebase specific implementation here
+    return Future<void>.value();
+  }
+
+  @override
+  Future<void> loginWithGoogle() {
+    // Firebase specific implementation here
+    return Future<void>.value();
+  }
+}
