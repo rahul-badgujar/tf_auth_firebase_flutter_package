2883408fff91b4673deddeeeb36e708591f4c735
Rahul Badgujar <rahulbadgujar1508@gmail.com>
2022-05-26 15:18:46 +0530

    exporting tf_auth_base package

--- lib/tf_auth_firebase.dart
+++ lib/tf_auth_firebase.dart
@@ -1,1 +1,7 @@
 library tf_auth_firebase;
+
+// exporting tf_base for basic data types and interfaces
+export 'package:tf_auth_base/tf_auth_base.dart';
+
+// exporting widgets and impl classes
+export 'src/tf_auth_firebase_impl.dart';
