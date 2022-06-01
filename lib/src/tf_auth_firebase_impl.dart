e68c78802bd1ccd356ff6d765c1f92a412c4d9a0
Rahul Badgujar <rahulbadgujar1508@gmail.com>
2022-06-02 00:41:34 +0530

    added impl for signup method

--- lib/src/tf_auth_firebase_impl.dart
+++ lib/src/tf_auth_firebase_impl.dart
@@ -6,6 +6,38 @@ class TfAuthFirebase extends TfAuth {
   TfAuthFirebase({required this.firebaseAuthInstance});
 
   @override
+  Future<TfAuthUser> signupWithEmailPassword(
+      {required String email, required String password}) async {
+    try {
+      final credential =
+          await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      final firebaseUser = credential.user;
+      if (firebaseUser == null) {
+        throw "Something went wrong";
+      }
+      final tfAuthUser = __tfAuthUserFromFirebaseUser(firebaseUser);
+      return tfAuthUser;
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        throw 'The password provided is too weak.';
+      } else if (e.code == 'email-already-in-use') {
+        throw 'The account already exists for that email.';
+      } else if (e.code == 'invalid-email') {
+        throw 'The email provided is invalid';
+      } else if (e.code == 'operation-not-allowed') {
+        throw 'Email Password Authentication not configured for this Firebase Project.';
+      } else {
+        throw "Something went wrong";
+      }
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  @override
   Future<TfAuthUser> forgotPasswordForEmail({required String email}) {
     // TODO: implement forgotPasswordForEmail
     throw UnimplementedError();
@@ -48,10 +80,15 @@ class TfAuthFirebase extends TfAuth {
     throw UnimplementedError();
   }
 
-  @override
-  Future<TfAuthUser> signupWithEmailPassword(
-      {required String email, required String password}) {
-    // TODO: implement signupWithEmailPassword
-    throw UnimplementedError();
+  static TfAuthUser __tfAuthUserFromFirebaseUser(User firebaseUser) {
+    final tfAuthUser = TfAuthUser(
+      uid: firebaseUser.uid,
+      displayName: firebaseUser.displayName,
+      email: firebaseUser.email,
+      username: firebaseUser.email,
+      isEmailVerified: true,
+      photoUrl: firebaseUser.photoURL,
+    );
+    return tfAuthUser;
   }
 }
