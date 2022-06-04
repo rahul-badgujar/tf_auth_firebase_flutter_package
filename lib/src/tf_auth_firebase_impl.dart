867c3f4b6fa001e57338c5c470a4c8208ed2cfc3
YashJ1707 <yashjaybhaye1707@gmail.com>
2022-06-04 10:29:22 +0530

    Added facebook login

--- lib/src/tf_auth_firebase_impl.dart
+++ lib/src/tf_auth_firebase_impl.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:tf_auth_firebase/tf_auth_firebase.dart';
 
@@ -119,9 +120,30 @@ class TfAuthFirebase extends TfAuth {
   }
 
   @override
-  Future<TfAuthUser> loginWithFacebook() {
-    // TODO: implement loginWithFacebook
-    throw UnimplementedError();
+  Future<TfAuthUser> loginWithFacebook() async {
+    try {
+      final LoginResult loginResult = await FacebookAuth.instance.login();
+
+      // Create a credential from the access token
+      final OAuthCredential facebookAuthCredential =
+          FacebookAuthProvider.credential(loginResult.accessToken!.token);
+
+      // Once signed in, return the UserCredential
+      final userCredential = await firebaseAuthInstance
+          .signInWithCredential(facebookAuthCredential);
+      final firebaseUser = userCredential.user;
+      if (firebaseUser == null) {
+        throw "Something went wrong";
+      }
+      final tfAuthUser = __tfAuthUserFromFirebaseUser(firebaseUser);
+      return tfAuthUser;
+    } on FirebaseAuthException catch (e) {
+      throw e.message.toString();
+    } catch (e) {
+      rethrow;
+    }
+    // // TODO: implement loginWithFacebook
+    // throw UnimplementedError();
   }
 
   @override
@@ -173,9 +195,14 @@ class TfAuthFirebase extends TfAuth {
   }
 
   @override
-  Future<TfAuthUser> logout() {
-    // TODO: implement logout
-    throw UnimplementedError();
+  Future<void> logout() async {
+    try {
+      await firebaseAuthInstance.signOut();
+    } on FirebaseAuthException catch (e) {
+      throw e.message.toString();
+    } catch (e) {
+      rethrow;
+    }
   }
 
   static TfAuthUser __tfAuthUserFromFirebaseUser(User firebaseUser) {
